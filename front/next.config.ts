2b44cdb244128aaad772166d5aa60a0b80fcdfb7
lauramussa <lauramussa94@gmail.com>
2025-12-12 23:10:01 -0300

    se modifico el next config

--- front/next.config.ts
+++ front/next.config.ts
@@ -13,6 +13,14 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.BACKEND_UR|| "http://localhost:3001"}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
